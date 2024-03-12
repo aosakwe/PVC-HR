822e94dd53aeb0910887a3da97dbaf7e364a0cb6
Adrien Osakwe <adrien.osakwe@mail.mcgill.ca>
2024-03-11 20:21:38 -0400

    Fixed mis-classification code

--- Scripts/Daily_variation.R
+++ Scripts/Daily_variation.R
@@ -11,11 +11,14 @@ source("./Scripts/PVCvsHR_stats.R")
 #source()
 
 
+
 ################################################
 #### 60 sec intervals ##########################
 
 {
-
+  pat.stats.total<-PVC_HR_patient_stats(version="Alt",interval=60,is.hourly=F,
+                                        use.pc.pvc = F, write.plot = F, day.number = NA)
+  
   pat.stats.per.day60<-PVC_HR_patient_stats(version="Alt",interval=60,is.hourly=F,
                                           use.pc.pvc = F, write.plot = F, day.number = 1)
   pat.stats.per.day60$day<-1
@@ -30,14 +33,20 @@ source("./Scripts/PVCvsHR_stats.R")
   pat.stats.per.day60<-pat.stats.per.day60[order(pat.stats.per.day60$patient_id,pat.stats.per.day60$day),]
   
   write.csv(pat.stats.per.day60,"Inputs/Daily_variation/Daily_patient_stats_60s.csv")
-
+  #percent miscategorized
+  ind<-match(pat.stats.per.day60$patient_id,pat.stats.total$patient_id)
+  pat.stats.per.day60$true_class<-pat.stats.total$class[ind]  
+  pat.stats.per.day60$miscatagorized<-pat.stats.per.day60$class != pat.stats.per.day60$true_class
+  pc_miss<-mean(pat.stats.per.day60$miscatagorized,na.rm=T)
+  paste(60," Percent Misclassified:",round(pc_miss,3))
   
 }
 
 ##############################################
 ##### 600 sec intervals ######################
 {
-  
+  pat.stats.total<-PVC_HR_patient_stats(version="Alt",interval=600,is.hourly=F,
+                                        use.pc.pvc = F, write.plot = F, day.number = NA)
   
   pat.stats.per.day600<-PVC_HR_patient_stats(version="Alt",interval=600,is.hourly=F,
                                              use.pc.pvc = F, write.plot = F, day.number = 1)
@@ -53,7 +62,12 @@ source("./Scripts/PVCvsHR_stats.R")
   pat.stats.per.day600<-pat.stats.per.day600[order(pat.stats.per.day600$patient_id,pat.stats.per.day600$day),]
   
   write.csv(pat.stats.per.day600,"Inputs/Daily_variation/Daily_patient_stats_600s.csv")
- 
+  #percent miscategorized
+  ind<-match(pat.stats.per.day600$patient_id,pat.stats.total$patient_id)
+  pat.stats.per.day600$true_class<-pat.stats.total$class[ind]  
+  pat.stats.per.day600$miscatagorized<-pat.stats.per.day600$class != pat.stats.per.day600$true_class
+  pc_miss<-mean(pat.stats.per.day600$miscatagorized,na.rm=T)
+  paste(600," Percent Misclassified:",round(pc_miss,3))
 }
 
 
@@ -61,6 +75,9 @@ source("./Scripts/PVCvsHR_stats.R")
 ## hourly
 
 {
+  pat.stats.total<-PVC_HR_patient_stats(version="Alt",interval=3600,is.hourly=F,
+                                        use.pc.pvc = F, write.plot = F, day.number = NA)
+  
   pat.stats.per.day1h<-PVC_HR_patient_stats(version="Alt",interval=3600,is.hourly=F,
                                             use.pc.pvc = F, write.plot = F, day.number = 1)
   pat.stats.per.day1h$day<-1
@@ -75,5 +92,10 @@ source("./Scripts/PVCvsHR_stats.R")
   pat.stats.per.day1h<-pat.stats.per.day1h[order(pat.stats.per.day1h$patient_id,pat.stats.per.day1h$day),]
   
   write.csv(pat.stats.per.day1h,"Inputs/Daily_variation/Daily_patient_stats_1hs.csv")
-  
+  #percent miscategorized
+  ind<-match(pat.stats.per.day1h$patient_id,pat.stats.total$patient_id)
+  pat.stats.per.day1h$true_class<-pat.stats.total$class[ind]  
+  pat.stats.per.day1h$miscatagorized<-pat.stats.per.day1h$class != pat.stats.per.day1h$true_class
+  pc_miss<-mean(pat.stats.per.day1h$miscatagorized,na.rm=T)
+  paste(3600," Percent Misclassified:",round(pc_miss,3))
 }
